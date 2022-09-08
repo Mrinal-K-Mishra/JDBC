fc1dc586323b604c7968e0c9bac826fb592c7b10
Mrinal-K-Mishra <107672054+Mrinal-K-Mishra@users.noreply.github.com>
2022-09-08 11:13:49 +0530

    Update ConnectionHelper.java

--- ConnectionHelper.java
+++ ConnectionHelper.java
@@ -1,5 +1,22 @@
 package JDBC;
 
-public class ConnectionHelper {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionHelper {
+static {
+		// try block
+		try {
+			// register my sql driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		// catch block	
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	// Coonection method to get connection from sql database
+	public static Connection con () throws SQLException {
+		return DriverManager.getConnection("jdbc:mysql://localhost:3507/lab","root","adrika@1897*06");
+	}
 }
